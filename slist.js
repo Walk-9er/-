eaaa2be6b52cce2503536e6b3027612963e0f5fb
Walk-9er <budnitsckyegor@gmail.com>
2024-12-11 17:58:07 +0300

    кр 2

---
+++ slist.js
@@ -0,0 +1,90 @@
+[
+    {
+      "id": "1",
+      "title": "Нокиа 3310",
+      "vendor_code": "111",
+      "description": "Легендарный телефон",
+      "price": 10000,
+      "img": "nokia.jpg"
+    },
+    {
+      "id": "2",
+      "title": "Старый телефон",
+      "vendor-code": "222",
+      "description": "Он электрический. Если вам кажется обратное - вы неправы",
+      "price": 20000,
+      "img": "old.jpg"
+    },
+    {
+      "id": "3",
+      "title": "Айфон 16 про макс",
+      "vendor-code": "333",
+      "description": "Наш магазин живёт в следующем году",
+      "price": 200000,
+      "img": "iphone.jpg"
+    },
+    {
+      "id": "4",
+      "title": "Вода",
+      "vendor-code": "444e",
+      "description": "Чтобы остудить электронику",
+      "price": 300,
+      "img": "water.jpg"
+    },
+    {
+      "id": "5",
+      "title": "Лампочка",
+      "vendor-code": "4445e",
+      "description": "Светится",
+      "price": 500,
+      "img": "lampochka.jpg"
+    },
+    {
+      "id": "6",
+      "title": "Мешок картошки",
+      "vendor-code": "4446e",
+      "description": "Не зря в магазин пришли",
+      "price": 600,
+      "img": "potato.jpg"
+    },
+    {
+      "id": "7",
+      "title": "Электромобиль",
+      "vendor-code": "4447e",
+      "description": "Ниже рынка! Только у нас!",
+      "price": 3000000,
+      "img": ""
+    },
+    {
+      "id": "8",
+      "title": "Купон на скидку в пятёрочке",
+      "vendor-code": "448e",
+      "description": "Мы самый универсальный магазин",
+      "price": 50,
+      "img": ""
+    },
+    {
+      "id": "9",
+      "title": "Часы Эппл Вотч",
+      "vendor-code": "444e",
+      "description": "Лучший выбор, чтобы показать статусность",
+      "price": 30000,
+      "img": ""
+    },
+    {
+      "id": "10",
+      "title": "Часы Сяоми",
+      "vendor-code": "44411e",
+      "description": "На случай, если быстро разберут Эппл Вотч",
+      "price": 15000,
+      "img": ""
+    },
+    {
+      "id": "11",
+      "title": "Плюшевый медведь",
+      "vendor-code": "44412e",
+      "description": "Подарок детям",
+    "price": 2000,
+    "img": ""
+  }
+]
